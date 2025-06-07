30acfd17fb018d7ad24c28d0286e675166d6f660
miriyalasupriya2112 <supriyamiriyala2112@gmail.com>
2025-06-07 10:22:08 +0530

    hhh 123

--- htz/htz-html/js/custom-slick.js
+++ htz/htz-html/js/custom-slick.js
@@ -84,22 +84,22 @@ jQuery(document).ready(function () {
 
   /* home banner Slider */
   jQuery(".feature-podcasts-slide-main").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 5,
+    slidesToScroll: 5,
     draggable: true,
     swipeToSlide: true,
     touchThreshold: 100,
     infinite: false,
     speed: 1000,
     dots: false,
-    arrows: false,
+    arrows: true,
     adaptiveHeight: true,
-    infinite: true,
+    infinite: false,
     variableWidth: true,
     prevArrow:
       '<div class="slick-arrow slick-prev flex flex-center" aria-label="Previous Arrow" role="button"><i class="fa-solid fa-circle-chevron-left"></i></div>',
     nextArrow:
-      '<div class="slick-arrow slick-next flex flex-center" aria-label="Next Arrow" role="button"><i class="fa-solid fa-circle-chevron-right"></i></div>',
+      '<div class="slick-arrow slick-next flex flex-center" aria-label="Next Arrow" role="button">Load More Audio</div>',
     responsive: [
       {
         breakpoint: 413,
