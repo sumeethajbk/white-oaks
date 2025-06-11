098add83333a1d1c9f122b481f4e604e8a36f3fd
miriyalasupriya2112 <supriyamiriyala2112@gmail.com>
2025-06-11 20:36:46 +0530

    hhh 123

--- htz/htz-html/js/custom-slick.js
+++ htz/htz-html/js/custom-slick.js
@@ -100,32 +100,7 @@ jQuery(document).ready(function () {
     ],
   });
 
-  /* day slider show slider */
+ 
 
-  jQuery(".day-slider").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    focusOnSelect: true,
-    arrows: true,
-    infinite: false,
-    variableWidth: true,
-    asNavFor: ".show-slider",
-    prevArrow:
-      '<div class="slick-arrow slick-prev flex flex-center" aria-label="Previous Arrow" role="button"><i class="fa-solid fa-circle-chevron-left"></i></div>',
-    nextArrow:
-      '<div class="slick-arrow slick-next flex flex-center" aria-label="Next Arrow" role="button"><i class="fa-solid fa-circle-chevron-right"></i></div>',
-  });
-
-  jQuery(".show-slider").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: ".day-slider",
-    arrows: true,
-    infinite: false,
-    variableWidth: true,
-    prevArrow:
-      '<div class="slick-arrow slick-prev flex flex-center" aria-label="Previous Arrow" role="button"><i class="fa-solid fa-circle-chevron-left"></i></div>',
-    nextArrow:
-      '<div class="slick-arrow slick-next flex flex-center" aria-label="Next Arrow" role="button"><i class="fa-solid fa-circle-chevron-right"></i></div>',
-  });
+ 
 });
